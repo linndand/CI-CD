851f039b133e1124a6186693d0ddcf8e9f601a0a
lindand <linnda.p.n@students.amikom.ac.id>
2025-05-09 16:49:25 +0700

    Tes Error

--- login.py
+++ login.py
@@ -3,7 +3,7 @@
 saved_username = "admin"
 saved_password = "12345"
 
-print("=== Sistem Login Sederhana By Linnda dan winda===")
+print("=== Sistem Login Sederhana By Linnda dan winda update===")
 username = input("Username: ")
 password = input("Password: ")
 
