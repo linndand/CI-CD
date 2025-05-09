c6d0f72f6c87c1abfa43a889ed81f8afb56285c5
lindand <linnda.p.n@students.amikom.ac.id>
2025-05-09 15:00:35 +0700

    first commit

---
+++ login.py
@@ -0,0 +1,13 @@
+# Sistem Login Sederhana
+
+saved_username = "admin"
+saved_password = "12345"
+
+print("=== Sistem Login Sederhana ===")
+username = input("Username: ")
+password = input("Password: ")
+
+if username == saved_username and password == saved_password:
+    print(f"\nLogin berhasil! Selamat datang, {username}.")
+else:
+    print("\nLogin gagal. Username atau password salah.")
